ad2396e5987501dad2ccd9a087c4950ace10a8c6
anabelendomenech <domenechanaa@gmail.com>
2024-09-23 21:11:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -13,35 +13,15 @@ document.addEventListener('DOMContentLoaded', () => {
                 // Aquí accedemos al producto que está dentro de la propiedad "products" del JSON
 
                 // Actualizar los detalles del producto en la página
-                document.getElementById('product-name').textContent = producto.name;
-                document.getElementById('category').textContent = `Categoría: ${producto.category}`;
-                document.getElementById('description').textContent = `Descripción: ${producto.description}`;
-                document.getElementById('sold').textContent = `Vendidos: ${producto.soldCount}`;
+                document.getElementById('company').textContent = 'Compañia: ${estrellas.company}';
+                document.getElementById('name').textContent = `Nombre: ${estrellas.name}`;
+                document.getElementById('numberrange').textContent =  ${estrellas.numberrange};
 
-                // Actualizar la imagen principal
-                const mainImage = document.querySelector('.main-image img');
-                mainImage.src = producto.images[0]; // Primera imagen como imagen principal
-
-                // Limpiar cualquier miniatura anterior
-                const thumbnailsContainer = document.querySelector('.product-images');
-                thumbnailsContainer.innerHTML = ''; // Limpiar las miniaturas previas
-
-                // Generar dinámicamente las miniaturas
-                producto.images.forEach((imagen, index) => {
-                    const imgElement = document.createElement('img');
-                    imgElement.src = imagen;
-                    imgElement.alt = `Imagen miniatura ${index + 1}`;
-                    imgElement.addEventListener('click', () => {
-                        // Cambiar la imagen principal al hacer clic en la miniatura
-                        mainImage.src = imagen;
-                    });
-                    thumbnailsContainer.appendChild(imgElement);
-                });
-            })
+               
             .catch(error => {
                 console.error('Error al obtener los datos del producto:', error);
             });
     } else {
-        console.error('Producto no encontrado en localStorage');
+        console.error('Comentario no encontrado en localStorage');
     }
 });
