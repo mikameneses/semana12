be6f61996ac3fa7c4346911cd8a103c8b8b80b7d
anabelendomenech <domenechanaa@gmail.com>
2024-09-23 21:44:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,4 @@
-//document.addEventListener('DOMContentLoaded', () => {
-    // Obtener el ID del comentatrio guardado en localStorage
-  //  const comentarioId = localStorage.getItem('id');
 
-    //if (comentarioId) {
-        // Dirección de la API
-      //  const apiUrl = `https://nataliasotelo.github.io/act-estrellas/estrellas.json`;
-
-        // Realizar la solicitud a la API para obtener los datos del producto
-        //fetch(apiUrl)
-          //  .then(response => response.json())
-            //.then(list => {
-               
-
-                // Actualizar los detalles del comentario en la página
-              //  document.getElementById('company').textContent = 'Compañia: ${estrellas.company}';
-                //document.getElementById('name').textContent = `Nombre: ${estrellas.name}`;
-                //document.getElementById('numberrange').textContent =  ${estrellas.numberrange};
-
-               
-            //.catch(error => {
-              //  console.error('Error al obtener los datos del producto:', error);
-            //});
-    //} else {
-      //  console.error('Comentario no encontrado en localStorage');
-    //}
-//});
 document.addEventListener('DOMContentLoaded', () => {
             const apiUrl = 'https://nataliasotelo.github.io/act-estrellas/estrellas.json';
             
